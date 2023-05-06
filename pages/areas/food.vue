2d01b508148f2742fe097a0f235bf396507571cf
Varun Sharman <varunsharman@Varuns-MBP.station>
2023-05-06 20:42:18 +0200

    service,food,health,tech pages

---
+++ pages/areas/food.vue
@@ -0,0 +1,30 @@
+<template>
+    <AppBody>
+        <div class="title-container">
+            <Subtitle :text="introTitle" />
+        </div>
+        <div>
+            <img src="~/assets/images/intro_pic.jpg"/>
+        </div>
+
+        <div class="title-container">
+            <Subtitle :text="teamTitle" />
+        </div>
+    </AppBody>
+</template>
+
+<script>
+import Subtitle from '~/components/Subtitle.vue';
+
+export default {
+    components: {
+        Subtitle
+    },
+    data() {
+        return {
+            introTitle: 'INTRO',
+            teamTitle: 'TEAM'
+        }
+    }
+}
+</script>
--- pages/areas/index.vue
+++ pages/areas/index.vue
@@ -38,28 +38,28 @@
         heading1: 'Service',
         text1: 'At our venture capital firm, we invest in a wide range of industries and sectors, from early-stage startups to established companies with a proven track record. We are well positioned to identify and invest in the most promising opportunities across a variety of sectors. Whether youre a startup looking to disrupt a market or an established company seeking to expand, we have the knowledge, resources, and network to help you achieve your goals.',
         buttonText1: 'Discover our Service Area',
-        navigateTo1: '/path/to/destination1',
+        navigateTo1: 'areas/service',
         
         // Data for the second AreaBadge component
         image2: 'https://source.unsplash.com/user/c_v_r/100x100',
         heading2: 'Health',
         text2: 'At our venture capital firm, we invest in a wide range of industries and sectors, from early-stage startups to established companies with a proven track record. We are well positioned to identify and invest in the most promising opportunities across a variety of sectors. Whether youre a startup looking to disrupt a market or an established company seeking to expand, we have the knowledge, resources, and network to help you achieve your goals.',
         buttonText2: 'Discover our Health Area',
-        navigateTo2: '/path/to/destination2',
+        navigateTo2: 'areas/health',
         
         // Data for the third AreaBadge component
         image3: 'https://source.unsplash.com/user/c_v_r/100x100',
         heading3: 'Tech',
         text3: 'At our venture capital firm, we have a deep passion for technology and believe in its potential to transform the world. We invest in a broad range of technology-focused companies, from enterprise software and cloud computing to consumer electronics and e-commerce. Our team of investment professionals has a strong track record of identifying and investing in innovative startups and established companies that are driving the next wave of technological disruption.',
         buttonText3: 'Discover our Tech Area',
-        navigateTo3: '/path/to/destination3',
+        navigateTo3: 'areas/tech',
         
         // Data for the fourth AreaBadge component
         image4: 'https://source.unsplash.com/user/c_v_r/1000x1000',
         heading4: 'Food',
         text4: 'At our venture capital firm, we believe that food is more than just sustenance – its an essential part of our culture, health, and wellbeing. Thats why we invest in companies that are revolutionizing the way we produce, distribute, and consume food. Our areas of focus include innovative food technology, sustainable agriculture, plant-based and alternative proteins, and more. If youre a food startup or an established company looking to transform the food industry, we would love to partner with you.',
         buttonText4: 'Discover our Food Area',
-        navigateTo4: '/path/to/destination4',
+        navigateTo4: 'areas/food',
       }
     },
   }
