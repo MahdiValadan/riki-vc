07b63e2e41298283b1995825207736b29f7eb8c1
Yujin Hong <ghddb113@hanyang.ac.kr>
2023-05-06 16:54:08 +0200

    changed header style and subtitle style

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="flex flex-col sticky top-0">
+  <header class="flex flex-col sticky top-0 font-roboto">
     <nav class="w-full px-16 py-4 flex flex-row justify-between items-center bg-white">
       <NuxtLink
         to="/"
-        class="font-bold text-xl"
+        class="font-bold text-4xl"
       >
         <span class="text-[#FF8F52]">RIKI</span> VC
       </NuxtLink>
-      <ul class="flex flex-row gap-8 font-medium">
+      <ul class="font-bold text-lg leading-7 flex flex-row items-center gap-12 py-0">
         <li class="transition hover:text-[#FF8F52]">
           <NuxtLink to="/persons">All Persons</NuxtLink>
         </li>
@@ -16,6 +16,12 @@
         </li>
         <li class="transition hover:text-[#FF8F52]">
           <NuxtLink to="/areas">All Areas</NuxtLink>
+        </li>
+        <li class="transition hover:text-[#FF8F52]">
+          <NuxtLink to="/about">About Us</NuxtLink>
+        </li>
+        <li class="transition hover:text-[#FF8F52]">
+          <NuxtLink to="/contact">Contact Us</NuxtLink>
         </li>
       </ul>
     </nav>
