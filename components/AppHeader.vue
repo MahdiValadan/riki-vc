5767f07fda62a7eb2a336538444dceb6780cf1cc
Yujin Hong <94971504+yujintonic@users.noreply.github.com>
2023-05-06 16:23:54 +0200

    Revert "change css into tailwind"

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,16 +2,14 @@
     <header class="flex flex-col sticky top-0">
       <nav class="w-full px-16 py-4 flex flex-row justify-between items-center bg-white" style="height: 100px;">
         <NuxtLink to="/" class="font-bold text-xl">
-          <div class="text-4xl">
+          <div class="homelink">
             <span class="text-[#FF8F52]">RIKI</span> VC
           </div>
         </NuxtLink>
-        <ul class="flex flex-row gap-8 font-bold text-lg leading-7 items-center py-0">
+        <ul class="flex flex-row gap-8 font-medium ul-font">
           <li class="transition hover:text-[#FF8F52]"><NuxtLink to="/persons">All Persons</NuxtLink></li>
           <li class="transition hover:text-[#FF8F52]"><NuxtLink to="/projects">All Projects</NuxtLink></li>
           <li class="transition hover:text-[#FF8F52]"><NuxtLink to="/areas">All Areas</NuxtLink></li>
-          <li class="transition hover:text-[#FF8F52]"><NuxtLink to="/about">About us</NuxtLink></li>
-          <li class="transition hover:text-[#FF8F52]"><NuxtLink to="/contact">Contact us</NuxtLink></li>
         </ul>
       </nav>
       <div class="bg-[#FF8F52] h-6"></div>
--- components/Subtitle.vue
+++ components/Subtitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="font-roboto font-bold text-5xl leading-tight text-black border-t-2 border-b-2 border-black py-2 text-center mt-6 mb-4">{{ text }}</h2>
+    <h2 class="title">{{ text }}</h2>
   </template>
   
   <script>
@@ -14,8 +14,18 @@
   </script>
   
   <style>
-  .font-roboto {
-  font-family: 'Roboto', sans-serif
+  .title {
+  font-family: 'Roboto', sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  line-height: 59px;
+  color: #000000;
+  border-top: 2px solid #000000;
+  border-bottom: 2px solid #000000;
+  padding: 0.5rem 0;
+  text-align: center;
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
   }
 
   .title-container {
