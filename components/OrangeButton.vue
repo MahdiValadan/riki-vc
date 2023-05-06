7680cfcbc415735fe644a0708bc4b85487e651b0
Yujin Hong <ghddb113@hanyang.ac.kr>
2023-05-06 19:04:46 +0200

    create team panel

---
+++ components/OrangeButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="bg-[#FF8F52] hover:bg-[#FF6D1E] hover:shadow-md text-white font-bold py-5 px-9 rounded">
+        {{ text }}
+    </button>
+  </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
