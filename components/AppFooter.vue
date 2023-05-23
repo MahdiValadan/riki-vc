dc231b3dfb9163ae719da35f541caa56372b1fae
Mahdi Valadan <94804009+MahdiValadan@users.noreply.github.com>
2023-05-24 01:02:17 +0200

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,28 +1,93 @@
 <template>
     <footer class="bg-[#454545] px-10 lg:px-20 py-8">
         <div class="flex flex-row flex-wrap gap-20 text-[#D9D9D9]">
+            <!-- Left Side -->
             <div class="w-full md:w-6/12">
+                <!-- LOGO -->
                 <div class="font-bold text-xl text-white">
                     <span class="text-[#06b6d4]">RIKI</span> VC
                 </div>
-                <p class="text-justify my-6">
-                    As a registered and licensed venture capital firm, we are fully compliant with all applicable
-                    regulations and standards governing our industry. Our team of professionals hold various licenses and
-                    certifications, including FINRA registrations, Series 7 and 63 licenses, and other relevant
-                    qualifications. We take pride in maintaining a culture of compliance and transparency, and we are
-                    dedicated to providing our investors with the best possible experience while minimizing risk and
-                    maximizing returns.
+                <!-- Copyright -->
+                <p class="text-justify mt-6">
+                    &#169; 2023 RIKI VC All Rights Reserved. <br>
+                    This Website is Inspired by
+                    <a
+                        class="text-[#06b6d4]"
+                        href="https://www.oltreimpact.com/"
+                    >
+                        Oltre Impact
+                    </a>
+                    as a University Project at the Politecnico di Milano. <br>
+                    Persons and Projects Images are taken from
+                    <a
+                        class="text-[#06b6d4]"
+                        href="https://www.oltreimpact.com/"
+                    >
+                        Oltre Impact.
+                    </a>
                 </p>
             </div>
-           
+            <!-- Right Side -->
             <div>
-                <h2 class="font-bold mt-3">Contact</h2>
-                <ul class="flex flex-col gap-4 mt-4">
-                    <li>26985 Brighton Lane, Lake Forest, CA</li>
-                    <li>support@rikivc.com</li>
-                    <li>+1 236 5489</li>
-                </ul>
-            </div>
+                <h2 class="font-bold">Contact</h2>
+                <ul class="flex flex-col gap-4 mt-6">
+                    <li class="flex flex-row gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-5 h-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                            />
+                        </svg>
+                        Piazza Leonardo da Vinci, 32, 20133 Milano MI, Italy
+                    </li>
+                    <li class="flex flex-row gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-5 h-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
+                            />
+                        </svg>
+                        support@rikivc.com
+                    </li>
+                    <li class="flex flex-row gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-5 h-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                            />
+                    </svg>
+
+                    +39 02 23991
+                </li>
+            </ul>
         </div>
-    </footer>
-</template>
+    </div>
+</footer></template>
